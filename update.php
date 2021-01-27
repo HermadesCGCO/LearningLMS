5757338abb4ac00322608c1c223ce228912d042a
HermadesCGCO <hermadescgco@disroot.org>
2021-01-27 18:13:08 +0000

    Agregado un TODO con las paginas pendientes por agregar

--- update.php
+++ update.php
@@ -6,7 +6,12 @@
 
 include "inc/config.php";
 
+echo "Actualizando \"courses\"";
+
+$conn->query("ALTER TABLE courses ADD featured VARCHAR(3) NOT NULL DEFAULT 'no' AFTER duration");
+
 echo "Actualizando \"courses_lessons\"<br>";
+
 $conn->query("ALTER TABLE courses_lessons ADD showOrder INT(255) NOT NULL AFTER video");
 
 ?>
