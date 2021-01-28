a9dbcdf2b9be2dc240cdcc293a936e8dbd3efcf9
HermadesCGCO <hermadescgco@disroot.org>
2021-01-28 19:50:55 +0000

    Añadida la base de datos y actualizado el .gitignore

---
+++ db.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 28, 2021 at 07:50 PM
+-- Server version: 10.5.8-MariaDB
+-- PHP Version: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `delta`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shortDesc` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `thumb` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `difficulty` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Principiantes',
+  `category` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `duration` varchar(4) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `featured` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'no',
+  `lastUpdated` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `students` int(255) NOT NULL DEFAULT 0,
+  `lessons` int(255) NOT NULL DEFAULT 0,
+  `tutor` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses_lessons`
+--
+
+CREATE TABLE `courses_lessons` (
+  `id` int(11) NOT NULL,
+  `name` varchar(125) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `video` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `showOrder` int(255) NOT NULL,
+  `sectionId` int(255) NOT NULL,
+  `courseId` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses_sections`
+--
+
+CREATE TABLE `courses_sections` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `courseId` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses_youlearn`
+--
+
+CREATE TABLE `courses_youlearn` (
+  `id` int(11) NOT NULL,
+  `course` int(255) NOT NULL,
+  `content` longtext COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `registrationDate` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `wantlearn` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ',',
+  `finished` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'no',
+  `isTutor` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'no'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses_lessons`
+--
+ALTER TABLE `courses_lessons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses_sections`
+--
+ALTER TABLE `courses_sections`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses_youlearn`
+--
+ALTER TABLE `courses_youlearn`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `courses_lessons`
+--
+ALTER TABLE `courses_lessons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `courses_sections`
+--
+ALTER TABLE `courses_sections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `courses_youlearn`
+--
+ALTER TABLE `courses_youlearn`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
