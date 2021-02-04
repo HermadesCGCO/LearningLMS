03c66a9951cf736bdbfbf791e1bb7341b8556fe9
HermadesCGCO <hermadescgco@disroot.org>
2021-02-04 18:58:32 +0000

    Actualizada la base de datos

--- db.sql
+++ db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 30, 2021 at 12:07 PM
--- Server version: 10.5.8-MariaDB
--- PHP Version: 7.4.14
+-- Generation Time: Feb 04, 2021 at 07:58 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,6 +36,8 @@ CREATE TABLE `courses` (
   `difficulty` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Principiantes',
   `category` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
   `duration` varchar(4) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `rating` float NOT NULL DEFAULT 0,
+  `ratings` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
   `featured` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'no',
   `lastUpdated` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `students` int(255) NOT NULL DEFAULT 0,
@@ -58,6 +60,21 @@ CREATE TABLE `courses_lessons` (
   `sectionId` int(255) NOT NULL,
   `courseId` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses_reviews`
+--
+
+CREATE TABLE `courses_reviews` (
+  `id` int(11) NOT NULL,
+  `student` varchar(255) NOT NULL,
+  `content` longtext NOT NULL,
+  `stars` int(1) NOT NULL DEFAULT 1,
+  `featured` varchar(3) NOT NULL DEFAULT 'no',
+  `course` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -86,6 +103,22 @@ CREATE TABLE `courses_youlearn` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `type` int(2) NOT NULL DEFAULT -1 COMMENT 'Type of the notification',
+  `receiver` varchar(255) NOT NULL COMMENT 'Name of the user who received the notification',
+  `sender` varchar(255) NOT NULL COMMENT 'Name of the user who is creating the notification',
+  `date` varchar(50) NOT NULL,
+  `dataInt` int(255) NOT NULL DEFAULT -1 COMMENT 'If the notification needs to store an int value, it should be here.',
+  `hasRead` varchar(3) NOT NULL DEFAULT 'no'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `student_progress`
 --
 
@@ -94,6 +127,7 @@ CREATE TABLE `student_progress` (
   `student` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `courseId` int(255) NOT NULL,
   `lessonId` int(255) NOT NULL,
+  `sectionId` int(255) NOT NULL,
   `completedLessons` int(10) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -122,6 +156,7 @@ CREATE TABLE `users` (
   `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unreadNotifications` int(255) NOT NULL DEFAULT 0,
   `registrationDate` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `wantlearn` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ',',
   `finished` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'no',
@@ -145,6 +180,12 @@ ALTER TABLE `courses_lessons`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `courses_reviews`
+--
+ALTER TABLE `courses_reviews`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `courses_sections`
 --
 ALTER TABLE `courses_sections`
@@ -154,6 +195,12 @@ ALTER TABLE `courses_sections`
 -- Indexes for table `courses_youlearn`
 --
 ALTER TABLE `courses_youlearn`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -191,6 +238,12 @@ ALTER TABLE `courses_lessons`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `courses_reviews`
+--
+ALTER TABLE `courses_reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `courses_sections`
 --
 ALTER TABLE `courses_sections`
@@ -200,6 +253,12 @@ ALTER TABLE `courses_sections`
 -- AUTO_INCREMENT for table `courses_youlearn`
 --
 ALTER TABLE `courses_youlearn`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
