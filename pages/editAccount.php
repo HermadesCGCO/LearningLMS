60f4d737f521debdfd6e5997ede8077921442abf
HermadesCGCO <hermadescgco@disroot.org>
2021-02-06 16:31:46 +0000

    Agregados comentarios

---
+++ pages/editAccount.php
@@ -0,0 +1,7 @@
+<?php
+
+// TODO: Editar cuenta e insertar/actualizar los datos en "userInfo". Si el
+// usuario ya tiene Información Aquí solamente se actualizan los datos
+// anteriores, si no, se crear nuevos datos.
+
+?>
